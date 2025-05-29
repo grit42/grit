13af2397580dac5a45f5a656443f6a9f0dbd34e2
Jonathan Rebourt <jonny@grit42.com>
2025-05-29 05:58:30 +0200

    more uniqueness checks in migration

--- modules/compounds/backend/db/migrate/20250521140707_grit_compounds_add_unique_indexes_to_unique_columns.rb
+++ modules/compounds/backend/db/migrate/20250521140707_grit_compounds_add_unique_indexes_to_unique_columns.rb
@@ -24,9 +24,17 @@ class GritCompoundsAddUniqueIndexesToUniqueColumns < ActiveRecord::Migration[7.2
     add_index :grit_compounds_batch_properties, :name, unique: true, name: 'idx_grit_compounds_batch_properties_on_name_unique'
     add_index :grit_compounds_batch_properties, :safe_name, unique: true, name: 'idx_grit_compounds_batch_properties_on_safe_name_unique'
 
+    duplicate_compound_names = duplicates(Grit::Compounds::Compound, :name)
+    raise DuplicateNameError.new("Duplicate compound names: #{duplicate_compound_names.join(", ")}") if duplicate_compound_names.any?
     add_index :grit_compounds_compounds, :name, unique: true, name: 'idx_grit_compounds_compounds_on_name_unique'
     add_index :grit_compounds_compounds, :number, unique: true, name: 'idx_grit_compounds_compounds_on_number_unique'
+
+    duplicate_synonym_names = duplicates(Grit::Compounds::Synonym, :name)
+    raise DuplicateNameError.new("Duplicate synonym names: #{duplicate_synonym_names.join(", ")}") if duplicate_synonym_names.any?
     add_index :grit_compounds_synonyms, :name, unique: true, name: 'idx_grit_compounds_compound_synonyms_on_name_unique'
+
+    duplicate_batch_names = duplicates(Grit::Compounds::Batch, :name)
+    raise DuplicateNameError.new("Duplicate batch names: #{duplicate_batch_names.join(", ")}") if duplicate_synonym_names.any?
     add_index :grit_compounds_batches, :name, unique: true, name: 'idx_grit_compounds_batches_on_name_unique'
     add_index :grit_compounds_batches, :number, unique: true, name: 'idx_grit_compounds_batches_on_number_unique'
   end
