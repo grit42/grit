e3718bb4f7cd2f7d13c708851c527009267784b6
Jonathan Rebourt <jonny@grit42.com>
2025-09-12 08:24:32 +0200

    add license information

--- modules/assays/frontend/lib/features/data-tables/data-table/columns/DataTableColumnSelector.tsx
+++ modules/assays/frontend/lib/features/data-tables/data-table/columns/DataTableColumnSelector.tsx
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2025 grit42 A/S. <https://grit42.com/>
+ *
+ * This file is part of @grit42/assays.
+ *
+ * @grit42/assays is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or  any later version.
+ *
+ * @grit42/assays is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * @grit42/assays. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import { Button } from "@grit42/client-library/components";
 import { createSearchParams, Link, useNavigate } from "react-router-dom";
 import {
