5f54c6732071d7235a6a752c1f77312ea4de2516
mrFlemming <jacob@grit42.com>
2025-11-25 11:30:06 +0100

    Limits

--- modules/assays/backend/db/migrate/20251113130627_limit_data_sheet_column_safename.rb
+++ modules/assays/backend/db/migrate/20251113130627_limit_data_sheet_column_safename.rb
@@ -3,6 +3,6 @@ class LimitDataSheetColumnSafename < ActiveRecord::Migration[7.2]
     change_column :grit_assays_assay_data_sheet_columns, :safe_name, :string, limit: 30
   end
   def down
-    change_column :grit_assays_assay_data_sheet_columns, :safe_name, :string, limit: 32
+    change_column :grit_assays_assay_data_sheet_columns, :safe_name, :string, limit: nil
   end
 end
