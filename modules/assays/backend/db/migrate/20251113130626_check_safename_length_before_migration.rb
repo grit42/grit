a41092a21aadf18a66057eb37019ac13f7f4fdb6
mrFlemming <jacob@grit42.com>
2025-11-25 14:40:54 +0100

    More descriptive error message on migration

--- modules/assays/backend/db/migrate/20251113130626_check_safename_length_before_migration.rb
+++ modules/assays/backend/db/migrate/20251113130626_check_safename_length_before_migration.rb
@@ -6,15 +6,18 @@ class CheckSafenameLengthBeforeMigration < ActiveRecord::Migration[7.2]
     data_table_column_exceeding_limit = Grit::Assays::DataTableColumn.where("LENGTH(safe_name) > ?", limit)    
 
     if data_sheet_column_exceeding_limit.any?
-      puts "WARNING: The following records in 'grit_assays_assay_data_sheet_columns' have 'safe_name' exceeding #{limit} characters:"
+      puts "WARNING: The following DataSheetColumn records have 'safe_name' exceeding #{limit} characters:"
       data_sheet_column_exceeding_limit.each do |record|
-        puts "  Record ID: #{record.id}, Current Length: #{record.safe_name.length}"
+        data_sheet = Grit::Assays::AssayDataSheetDefinition.find(record.assay_data_sheet_definition_id)
+        model = Grit::Assays::AssayModel.find(data_sheet.assay_model_id)
+        puts "  Model: #{model.name}, Sheet: #{data_sheet.name}, Column safe_name: #{record.safe_name}, Current Length: #{record.safe_name.length}"
       end
     end
     if data_table_column_exceeding_limit.any?
-      puts "WARNING: The following records in 'grit_assays_data_table_columns' have 'safe_name' exceeding #{limit} characters:"
+      puts "WARNING: The following DataTableColumn records have 'safe_name' exceeding #{limit} characters:"
       data_table_column_exceeding_limit.each do |record|
-        puts "  Record ID: #{record.id}, Current Length: #{record.safe_name.length}"
+        data_table = Grit::Assays::DataTable.find(record.data_table_id)
+        puts "  Data table: #{data_table.name}, Column safe_name: #{record.safe_name}, Current Length: #{record.safe_name.length}"
       end
     end
     if data_sheet_column_exceeding_limit.any? or data_table_column_exceeding_limit.any?
