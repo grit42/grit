7f273522d2c69dfb1060f3f5f53b9889e7b55dc0
Rasmus Borup <rasmus@grit42.com>
2025-12-04 15:57:00 +0100

    Update moleculeviewer

--- modules/compounds/frontend/lib/pages/compounds/[id]/compound-cv/index.tsx
+++ modules/compounds/frontend/lib/pages/compounds/[id]/compound-cv/index.tsx
@@ -17,134 +17,165 @@
  */
 
 import { ErrorPage, Spinner, Surface } from "@grit42/client-library/components";
+import { useParams } from "react-router-dom";
 import {
-  EntityData,
-  EntityProperties,
-  useEditEntityMutation,
-  useDestroyEntityMutation,
-  useHasRoles,
-} from "@grit42/core";
-import { useMemo, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import {
-  useForm,
-  Form,
-  FormControls,
-  FormField,
-  genericErrorHandler,
-  getVisibleFieldData,
-} from "@grit42/form";
-import { useCompound, useCompoundFields } from "../../../../queries/compounds";
+  CompoundData,
+  useCompound,
+  useCompoundFields,
+} from "../../../../queries/compounds";
 import { AsyncMoleculeViewer } from "../../../../components/MoleculeViewer";
 import styles from "./details.module.scss";
 
-const CompoundDetails = () => {
-  const canCrud = useHasRoles([
-    "Administrator",
-    "CompoundAdministrator",
-    "CompoundUser",
-  ]);
+// const CompoundDetails = () => {
+//   const canCrud = useHasRoles([
+//     "Administrator",
+//     "CompoundAdministrator",
+//     "CompoundUser",
+//   ]);
+//   const { id } = useParams() as { id: string };
+//   const { data: compound } = useCompound(id);
+//   const { data: fields } = useCompoundFields(compound?.compound_type_id);
+
+//   const compoundTypeFields = useMemo(
+//     () =>
+//       fields
+//         ?.filter(
+//           (f) =>
+//             (f.compound_type_id === null ||
+//               f.compound_type_id === compound?.compound_type_id) &&
+//             f.name !== "molecule",
+//         )
+//         .map((f) =>
+//           ["compound_type_id", "number"].includes(f.name)
+//             ? { ...f, disabled: true }
+//             : { ...f, disabled: !canCrud },
+//         ) ?? [],
+//     [fields, compound],
+//   );
+
+//   const [showGeneralInfo, setShowGeneralInfo] = useState(false);
+//   const [showCalcProps, setShowCalcProps] = useState(false);
+//   const [showCharacterization, setShowCharacterization] = useState(false);
+
+//   const OrderGeneralInfo = [
+//     "Number",
+//     "Name",
+//     "Molecular formula",
+//     "inchi",
+//     "inchi key",
+//   ];
+//   const OrderGeneralInfoLC = OrderGeneralInfo.map((s) => s.toLowerCase());
+//   const AllowedGeneralInfo = new Set(OrderGeneralInfoLC);
+
+//   const OrderCalculatedProperties = [
+//     "MW",
+//     "logP",
+//     "Hydrogen Bond Donor Count",
+//     "Hydrogen Bond Acceptor Count",
+//   ];
+//   const OrderCalculatedPropertiesLC = OrderCalculatedProperties.map((s) =>
+//     s.toLowerCase(),
+//   );
+//   const AllowedCalculatedProperties = new Set(OrderCalculatedPropertiesLC);
+
+//   function filteredFields(allowedSet: Set<string>, orderArray: string[]) {
+//     const filtered = (compoundTypeFields ?? [])
+//       .filter((f) => allowedSet.has(String(f.display_name).toLowerCase()))
+//       .sort(
+//         (a, b) =>
+//           orderArray.indexOf(String(a.display_name).toLowerCase()) -
+//           orderArray.indexOf(String(b.display_name).toLowerCase()),
+//       );
+//     return filtered.map((f) => (
+//       <li key={f.name}>
+//         {f.display_name}: {compound?.[f.name] ?? ""}
+//       </li>
+//     ));
+//   }
+
+//   const generalInfoItems = useMemo(
+//     () => filteredFields(AllowedGeneralInfo, OrderGeneralInfoLC),
+//     [compound, compoundTypeFields],
+//   );
+//   const CalculatedPropertiesInfoItems = useMemo(
+//     () =>
+//       filteredFields(AllowedCalculatedProperties, OrderCalculatedPropertiesLC),
+//     [compound, compoundTypeFields],
+//   );
+
+//   const GeneralInfoToggle = () => setShowGeneralInfo((v) => !v);
+//   const CalcPropToggle = () => setShowCalcProps((v) => !v);
+//   const CharacterizationToggle = () => setShowCharacterization((v) => !v);
+
+//   return (
+//     <div className={styles.container}>
+//       <Surface style={{ width: "100%" }}>
+//         {compound?.molecule && (
+//           <div className={styles.moleculeContainer}>
+//             <AsyncMoleculeViewer molfile={compound.molecule} />
+//           </div>
+//         )}
+
+//         <button onClick={GeneralInfoToggle} aria-expanded={showGeneralInfo}>
+//           General Information
+//         </button>
+//         {showGeneralInfo && (
+//           <div className={styles.generalInfo}>
+//             <ul style={{ listStyleType: "none", padding: 0, margin: 0 }}>
+//               {generalInfoItems}
+//             </ul>
+//           </div>
+//         )}
+
+//         <button onClick={CalcPropToggle} aria-expanded={showCalcProps}>
+//           Calculated Properties
+//         </button>
+
+//         {showCalcProps && (
+//           <div className={styles.calcProps}>
+//             <ul style={{ listStyleType: "none", padding: 0, margin: 0 }}>
+//               {CalculatedPropertiesInfoItems}
+//             </ul>
+//           </div>
+//         )}
+
+//         <button
+//           onClick={CharacterizationToggle}
+//           aria-expanded={showCharacterization}
+//         >
+//           Characterization
+//         </button>
+//       </Surface>
+//     </div>
+//   );
+// };
+
+const MoleculeViewer = ({ compound }: { compound: CompoundData }) => {
+  return (
+    <div className={styles.moleculeContainer}>
+      {compound.molecule ? (
+        <AsyncMoleculeViewer molfile={compound.molecule} />
+      ) : (
+        "No molecule data available."
+      )}
+    </div>
+  );
+};
+
+const CompoundCV = () => {
   const { id } = useParams() as { id: string };
   const { data: compound } = useCompound(id);
-  const { data: fields } = useCompoundFields(compound?.compound_type_id);
-
-  const compoundTypeFields = useMemo(
-    () =>
-      fields
-        ?.filter(
-          (f) =>
-            (f.compound_type_id === null ||
-              f.compound_type_id === compound?.compound_type_id) &&
-            f.name !== "molecule",
-        )
-        .map((f) =>
-          ["compound_type_id", "number"].includes(f.name)
-            ? { ...f, disabled: true }
-            : { ...f, disabled: !canCrud },
-        ) ?? [],
-    [fields, compound],
-  );
-
-  const [showGeneralInfo, setShowGeneralInfo] = useState(false);
-  const [showCalcProps, setShowCalcProps] = useState(false);
-  const [showCharacterization, setShowCharacterization] = useState(false);
-
-  const OrderGeneralInfo = ['Number', 'Name', 'Molecular formula', 'inchi', 'inchi key'];
-  const OrderGeneralInfoLC = OrderGeneralInfo.map(s => s.toLowerCase());
-  const AllowedGeneralInfo = new Set(OrderGeneralInfoLC);
-
-
-  const OrderCalculatedProperties = ['MW', 'logP', 'Hydrogen Bond Donor Count', 'Hydrogen Bond Acceptor Count'];
-  const OrderCalculatedPropertiesLC = OrderCalculatedProperties.map(s => s.toLowerCase());
-  const AllowedCalculatedProperties = new Set(OrderCalculatedPropertiesLC);
-
-  function filteredFields(allowedSet: Set<string>, orderArray: string[]) {
-    const filtered = (compoundTypeFields ?? [])
-      .filter(f => allowedSet.has(String(f.display_name).toLowerCase()))
-      .sort(
-        (a, b) =>
-          orderArray.indexOf(String(a.display_name).toLowerCase()) -
-          orderArray.indexOf(String(b.display_name).toLowerCase())
-      );
-    return filtered.map(f => (
-      <li key={f.name}>
-        {f.display_name}: {compound?.[f.name] ?? ''}
-      </li>
-    ));
-  }
-
-  const generalInfoItems = useMemo(() => filteredFields(AllowedGeneralInfo, OrderGeneralInfoLC), [compound, compoundTypeFields]);
-  const CalculatedPropertiesInfoItems = useMemo(() => filteredFields(AllowedCalculatedProperties, OrderCalculatedPropertiesLC), [compound, compoundTypeFields]);
-
-
-
-  const GeneralInfoToggle = () => setShowGeneralInfo(v => !v);
-  const CalcPropToggle = () => setShowCalcProps(v => !v);
-  const CharacterizationToggle = () => setShowCharacterization(v => !v);
 
   return (
-
     <div className={styles.container}>
       <Surface style={{ width: "100%" }}>
-        {compound?.molecule && (
-          <div className={styles.moleculeContainer}>
-            <AsyncMoleculeViewer molfile={compound.molecule} />
-          </div>
-        )}
-
-        <button onClick={GeneralInfoToggle} aria-expanded={showGeneralInfo}>
-          General Information
-        </button>
-        {showGeneralInfo && (
-        <div className={styles.generalInfo}>
-          <ul style={{ listStyleType: 'none', padding: 0, margin: 0 }}>
-            {generalInfoItems}
-          </ul>
-        </div>
-      )}
-
-      <button onClick={CalcPropToggle} aria-expanded={showCalcProps}>
-        Calculated Properties
-      </button>
-
-      {showCalcProps && (
-        <div className={styles.calcProps}>
-          <ul style={{ listStyleType: 'none', padding: 0, margin: 0 }}>
-            {CalculatedPropertiesInfoItems}
-          </ul>
-        </div>
-      )}
-
-      <button onClick={CharacterizationToggle} aria-expanded={showCharacterization}>
-        Characterization
-      </button>
-
+        {compound && <MoleculeViewer compound={compound} />}
       </Surface>
     </div>
   );
 };
 
-const CompoundDetailsPage = () => {
+const CompoundCVPage = () => {
   const {
     data: fields,
     isLoading: isFieldsLoading,
@@ -160,7 +191,7 @@ const CompoundDetailsPage = () => {
     return <ErrorPage error={fieldsError} />;
   }
 
-  return <CompoundDetails />;
+  return <CompoundCV />;
 };
 
-export default CompoundDetailsPage;
+export default CompoundCVPage;
