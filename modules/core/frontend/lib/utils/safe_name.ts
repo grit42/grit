6b29cc3786d15eee1caf89d3d74c9b57110f530b
Jonathan Rebourt <jonny@grit42.com>
2026-02-10 11:43:26 +0100

    fix core tosafeidentifier

--- modules/core/frontend/lib/utils/safe_name.ts
+++ modules/core/frontend/lib/utils/safe_name.ts
@@ -24,5 +24,5 @@ export function toSafeIdentifier(input: string, replacement = "_"): string {
     .trim()
     .replace(new RegExp("[^a-z0-9]", "g"), replacement);
 
-  return /^[0-9]/.test(normalized) ? `__${normalized}` : normalized;
+  return (/^[0-9]|^.[0-9]/.test(normalized) ? `__${normalized}` : normalized).slice(0, 30);
 }
