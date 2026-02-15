19cc163d229038b9966fde567947e8f64850e3ce
Jonathan Rebourt <jonny@grit42.com>
2026-02-15 08:26:32 +0100

    clean up grit entity record

--- modules/core/backend/app/models/concerns/grit/core/grit_entity_record.rb
+++ modules/core/backend/app/models/concerns/grit/core/grit_entity_record.rb
@@ -21,6 +21,10 @@ require "grit/core/entity_mapper"
 module Grit::Core::GritEntityRecord
   extend ActiveSupport::Concern
 
+  # ==========================================================================
+  # Setup & Configuration
+  # ==========================================================================
+
   included do
     @db_foreign_keys = nil
     @db_indexes = nil
@@ -52,31 +56,63 @@ module Grit::Core::GritEntityRecord
     end
 
     validate :numbers_in_range
-
-    def numbers_in_range
-      self.class.columns.each do |column|
-        next if ![ :integer, :float ].include?(column.sql_type_metadata.type) or column.sql_type_metadata.sql_type.end_with? "[]"
-
-        if column.sql_type_metadata.type == :integer && self[column.name].present? && (self[column.name].to_i < -(2**53-1) || self[column.name].to_i > 2**53-1)
-          errors.add(column.name, "is out of range")
-        end
-      end
-    end
-
     before_save :set_updater
+  end
 
-    def set_updater
-      unless self.class.name == "Grit::Core::User"
-        current_user_login = Grit::Core::User.current.login
-        self.created_by = current_user_login if self.new_record?
-        self.updated_by = current_user_login
+  # ==========================================================================
+  # Instance Methods
+  # ==========================================================================
+
+  def numbers_in_range
+    self.class.columns.each do |column|
+      next if ![ :integer, :float ].include?(column.sql_type_metadata.type) or column.sql_type_metadata.sql_type.end_with? "[]"
+
+      if column.sql_type_metadata.type == :integer && self[column.name].present? && (self[column.name].to_i < -(2**53-1) || self[column.name].to_i > 2**53-1)
+        errors.add(column.name, "is out of range")
       end
     end
   end
 
-  # The methods added inside the class_methods block (or, ClassMethods module)
-  # become the class methods on the including class.
+  def set_updater
+    unless self.class.name == "Grit::Core::User"
+      current_user_login = Grit::Core::User.current.login
+      self.created_by = current_user_login if self.new_record?
+      self.updated_by = current_user_login
+    end
+  end
+
+  # ==========================================================================
+  # Class Methods
+  # ==========================================================================
+
   class_methods do
+    # ------------------------------------------------------------------------
+    # DSL Methods (Model Configuration)
+    # ------------------------------------------------------------------------
+
+    private
+
+    def display_columns(display_columns)
+      @display_columns = display_columns.is_a?(String) ? [ display_columns ] : display_columns
+    end
+
+    alias_method :display_column, :display_columns
+
+    def entity_crud_with(create: nil, read: nil, update: nil, destroy: nil)
+      @entity_crud = {
+        create: create,
+        read: read,
+        update: update,
+        destroy: destroy
+      }
+    end
+
+    public
+
+    # ------------------------------------------------------------------------
+    # Database Introspection
+    # ------------------------------------------------------------------------
+
     def foreign_keys
       @db_foreign_keys ||= ActiveRecord::Base.connection.foreign_keys(self.table_name)
     end
@@ -122,6 +158,10 @@ module Grit::Core::GritEntityRecord
         property
       end
     end
+
+    # ------------------------------------------------------------------------
+    # Entity Metadata (Properties / Fields / Columns)
+    # ------------------------------------------------------------------------
 
     def display_properties
       @display_properties ||= @display_columns&.map { |name| self.db_properties.find { |p| p[:name] == name } } || []
@@ -204,6 +244,14 @@ module Grit::Core::GritEntityRecord
       @entity_columns ||= self.entity_columns_from_properties(self.db_properties)
     end
 
+    def entity_crud
+      @entity_crud
+    end
+
+    # ------------------------------------------------------------------------
+    # Query Building (Scopes)
+    # ------------------------------------------------------------------------
+
     def detailed_scope(params = nil)
       query = self.from(self.table_name)
       self.columns.each do |column|
@@ -230,28 +278,12 @@ module Grit::Core::GritEntityRecord
       self.detailed.where("#{self.table_name}.id IN (SELECT record_id FROM grit_core_load_set_block_loaded_records WHERE grit_core_load_set_block_loaded_records.load_set_block_id = ?)", params[:load_set_block_id].to_i).order(:created_at)
     end
 
-    def entity_crud
-      @entity_crud
-    end
+    # ------------------------------------------------------------------------
+    # Loader Support
+    # ------------------------------------------------------------------------
 
     def loader_find_by!(prop, value, **args)
       find_by!({ prop => value })
-    end
-
-    private
-    def display_columns(display_columns)
-      @display_columns = display_columns.is_a?(String) ? [ display_columns ] : display_columns
-    end
-
-    alias_method :display_column, :display_columns
-
-    def entity_crud_with(create: nil, read: nil, update: nil, destroy: nil)
-      @entity_crud = {
-        create: create,
-        read: read,
-        update: update,
-        destroy: destroy
-      }
     end
   end
 end
