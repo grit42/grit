e7311473da7ca8a3f29b21f0eaf1ed6783d67c0b
Jonathan Rebourt <jonny@grit42.com>
2026-04-14 09:34:45 +0200

    LoadingPage component

---
+++ packages/frontend/client-library/lib/components/LoadingPage/index.tsx
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2025 grit42 A/S. <https://grit42.com/>
+ *
+ * This file is part of @grit42/client-library.
+ *
+ * @grit42/client-library is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or  any later version.
+ *
+ * @grit42/client-library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * @grit42/client-library. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { PropsWithChildren } from "react";
+import styles from "./loadingPage.module.scss";
+import Spinner from "../Spinner";
+
+interface LoadingPageProps {
+  message?: string | null;
+}
+
+const LoadingPage = ({
+  message,
+  children,
+}: PropsWithChildren<LoadingPageProps>) => {
+  return (
+    <div className={styles.container}>
+      <Spinner className={styles.icon} />
+      <div className={styles.content}>
+        <h3>{message ?? "Loading..."}</h3>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default LoadingPage;
--- packages/frontend/client-library/lib/components/index.ts
+++ packages/frontend/client-library/lib/components/index.ts
@@ -41,6 +41,8 @@ export * from "./Dropdown";
 
 export { default as ErrorPage } from "./ErrorPage";
 
+export { default as LoadingPage } from "./LoadingPage";
+
 export { default as FileInput } from "./FileInput";
 export * from "./FileInput";
 
